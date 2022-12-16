556e8e6fd95e6497aac836368ce749586d1894e3
James Davies <jdavies@mpia.de>
2022-12-16 12:16:03 +0100

    Fix console scripts

--- setup.py
+++ setup.py
@@ -70,20 +70,20 @@ setup(
         ),
         Extension(
             'jwst.cube_build.cube_match_internal',
-            ['jwst/cube_build/src/cube_match_internal.c','jwst/cube_build/src/cube_utils.c'],
+            ['jwst/cube_build/src/cube_match_internal.c', 'jwst/cube_build/src/cube_utils.c'],
             include_dirs=include_dirs,
             define_macros=define_macros
         ),
         Extension(
             'jwst.cube_build.cube_match_sky_pointcloud',
-            ['jwst/cube_build/src/cube_match_sky_pointcloud.c','jwst/cube_build/src/cube_utils.c',
+            ['jwst/cube_build/src/cube_match_sky_pointcloud.c', 'jwst/cube_build/src/cube_utils.c',
              'jwst/cube_build/src/cube_dq_utils.c'],
             include_dirs=include_dirs,
             define_macros=define_macros
         ),
         Extension(
             'jwst.cube_build.cube_match_sky_driz',
-            ['jwst/cube_build/src/cube_match_sky_driz.c','jwst/cube_build/src/cube_utils.c',
+            ['jwst/cube_build/src/cube_match_sky_driz.c', 'jwst/cube_build/src/cube_utils.c',
              'jwst/cube_build/src/cube_dq_utils.c'],
             include_dirs=include_dirs,
             define_macros=define_macros
